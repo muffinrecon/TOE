179185d9effbeb07876dc1401f751f2725481b87
ClementineB <clementine.barbet@live.fr>
2014-04-04 15:59:43 -0400

    Revert "WIP modifying struct tcp_ctrl"
    This reverts commit a723d1b1a00a1a64d26af6e485b52effde9eb47e.

--- tcp.h
+++ tcp.h
@@ -23,26 +23,28 @@
 #define IP4_HDRLEN 20		// IPv4 header length
 #define TCP_HDRLEN 20		// TCP header length	
 
-struct tcp_ctrl {
+struct tcp_pcb {
 	int sd;
 	char *interface, *target, *src_ip, *dst_ip;
 	uint8_t *src_mac, *dst_mac, *ether_frame_in, *ether_frame_out;
+	int *ip_flags, *tcp_flags;
+	uint16_t in_port, out_port;
 	struct sockaddr_ll device;
-	uint16_t sport, dport;
-	int seq, ack, mtu;
-	struct tcphdr *tcphdr;
-	struct ip *iphdr; 
+	int seq;
+	uint16_t sport;
+	uint16_t dport; 
 };
 
+void tcp_tmr(void);
 
 // connection
-struct tcp_ctrl *tcp_new(void);
-int tcp_bind(struct tcp_ctrl*, char*, uint16_t, char*);
-struct tcp_ctrl *tcp_listen(struct tcp_ctrl *);
-void tcp_accept(struct tcp_ctrl *);
-void tcp_connect(struct tcp_ctrl *, char *);
+struct tcp_pcb *tcp_new(void);
+int tcp_bind(struct tcp_pcb*, char*, uint16_t, char*);
+struct tcp_pcb *tcp_listen(struct tcp_pcb *);
+void tcp_accept(struct tcp_pcb *);
+int tcp_connect(struct tcp_pcb *, char *);
 
 // Sending TCP data 
-void tcp_sent(struct tcp_ctrl *);
+void tcp_sent (struct tcp_pcb *);
 void tcp_sndbuf(void);
 void tcp_write(void);
