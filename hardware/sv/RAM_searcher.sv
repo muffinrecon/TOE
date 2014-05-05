29cc7028975c6ea60759935b5d84976839557359
hkustliqi <hkustliqi@gmail.com>
2014-05-05 15:38:33 -0400

    Update RAM_searcher.sv

--- hardware/sv/RAM_searcher.sv
+++ hardware/sv/RAM_searcher.sv
@@ -1,3 +1,8 @@
+//Note: before setting rs_rq == 1, meaning initialzing a new connection, input data rs_ip_src etc.. has to be set for at least 2 clock 
+//cycles in advacne. This was tested in ModelSim to make sure correct timing.
+
+//deletion of a connection not done yet
+
 module RAM_searcher (input logic 		rs_clk,
 		     input logic 		rs_rst,
 		     input logic [1:0] 		rs_rq,
